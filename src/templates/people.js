e309dc9574b9d9abfdf03d21fd01017806c205d0
David Wnek <davidwnek@gmail.com>
2020-01-02 12:48:07 -0600

    Changed static query from hook to component

--- src/templates/people.js
+++ src/templates/people.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {graphql, useStaticQuery} from 'gatsby';
+import {graphql, StaticQuery} from 'gatsby';
 import Well from '../Components/Well';
 import Layout from '../Components/Layout';
 import get from 'lodash/get';
@@ -8,22 +8,26 @@ import sortBy from 'lodash/sortBy';
 import filter from 'lodash/filter';
 
 function People({type}) {
-  const data = useStaticQuery(query);
-  const people = sortBy(filter(get(data, 'prismic.allPersons.edges', []), person =>  person.node.type === type), person => person.node.order);
-
   return (
-    <Layout>
-      <Well>
-        {people.map((node, key) => {
-          return <Person node={node.node} key={key} pull={key % 2 === 0 ? 'right' : 'left'}/>
-        })}
-      </Well>
-    </Layout>
-  );
+    <StaticQuery query={query} render={data => {
+      const people = sortBy(filter(get(data, 'prismic.allPersons.edges', []), person =>  person.node.type === type), person => person.node.order);
+
+      return (
+        <Layout>
+          <Well>
+            {people.map((node, key) => {
+              return <Person node={node.node} key={key} pull={key % 2 === 0 ? 'right' : 'left'}/>
+            })}
+          </Well>
+        </Layout>
+      );
+    }}
+    />
+  )
 }
 
 const query = graphql`
-    query boardQuery {
+    query PeopleQuery {
         prismic {
             allPersons {
                 edges {

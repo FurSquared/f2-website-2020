ac09e5573651f9440afd9d674184b65fe068274e
David Wnek <davidwnek@gmail.com>
2020-01-02 00:26:52 -0600

    Changed query hook to component

--- src/pages/theme.js
+++ src/pages/theme.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {graphql, useStaticQuery, Link} from 'gatsby';
+import {graphql, useStaticQuery, Link, StaticQuery} from 'gatsby';
 import Well from '../Components/Well';
 import Layout from '../Components/Layout';
 import get from 'lodash/get';
@@ -21,21 +21,26 @@ const ThemeStyles = styled.div`
 `;
 
 function Theme() {
-  const data = useStaticQuery(query);
-
-  const nodes = get(data, 'prismic.allThemes.edges[0].node');
-
   return (
-    <Layout>
-      <Well>
-        <Title node={nodes.title[0]}/>
-        <ThemeStyles>
-          {RichText.render(nodes.invitation)}
-          <Link to={`/corporation`}>Join u<strong>s.</strong></Link><p> The future is now. Reality Cubed.</p>
-        </ThemeStyles>
-        {RichText.render(nodes.content)}
-      </Well>
-    </Layout>
+    <StaticQuery
+      query={query}
+      render={data => {
+
+        const nodes = get(data, 'prismic.allThemes.edges[0].node');
+        return (
+        <Layout>
+          <Well>
+            <Title node={nodes.title[0]}/>
+            <ThemeStyles>
+              {RichText.render(nodes.invitation)}
+              <Link to={`/corporation`}>Join u<strong>s.</strong></Link><p> The future is now. Reality Cubed.</p>
+            </ThemeStyles>
+            {RichText.render(nodes.content)}
+          </Well>
+        </Layout>
+        );
+      }}
+      />
   );
 }
 
